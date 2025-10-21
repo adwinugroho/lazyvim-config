dc21b784484f15413ffec7a1631fe8e173f719eb
adwinugroho <adwinnugroho16@gmail.com>
2025-10-21 10:43:00 +0700

    fix: update colorschme & add dotenv

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,7 +22,7 @@ return {
         light = "latte",
         dark = "mocha",
       },
-      transparent_background = false,
+      transparent_background = true,
       show_end_of_buffer = false,
       term_colors = false,
       integrations = {
